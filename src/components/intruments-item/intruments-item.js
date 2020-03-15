cec3598131d7b39c51775a1e3ebaa7084bc8a552
Alex Boicu <boicualexandru@mail.com>
2020-03-15 19:59:37 +0200

    InstrumentsItem Component - es lint fix

--- src/components/intruments-item/intruments-item.js
+++ src/components/intruments-item/intruments-item.js
@@ -2,22 +2,31 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./intruments-item.scss";
 
-export const InstrumentsItem = ({ color, title, content, ctaText, ctaOnClick }) => {
-  return <div className="info-card">
-      <div className="card-title" style={color && {borderColor: color}}>
+export const InstrumentsItem = ({
+  color,
+  title,
+  content,
+  ctaText,
+  ctaOnClick
+}) => {
+  return (
+    <div className="info-card">
+      <div className="card-title" style={color && { borderColor: color }}>
         {title}
       </div>
       <div className="card-content">
-        {
-          content &&
-          <p>{content}</p>
-        }
-        {
-          ctaText &&
-          <button style={color && {backgroundColor: color}} onClick={ctaOnClick}>{ctaText}</button>
-        }
+        {content && <p>{content}</p>}
+        {ctaText && (
+          <button
+            style={color && { backgroundColor: color }}
+            onClick={ctaOnClick}
+          >
+            {ctaText}
+          </button>
+        )}
       </div>
     </div>
+  );
 };
 
 InstrumentsItem.propTypes = {
--- src/components/intruments-item/intruments-item.stories.js
+++ src/components/intruments-item/intruments-item.stories.js
@@ -4,36 +4,44 @@ import { InstrumentsItem } from "./intruments-item";
 
 export default { title: "InstrumentsItem" };
 
-export const basic = () => 
-  <div style={{width: 500}}>
-    <InstrumentsItem 
-      color="#FF1616" 
-      title="Stiri oficiale si informații la zi" 
+export const basic = () => (
+  <div style={{ width: 500 }}>
+    <InstrumentsItem
+      color="#FF1616"
+      title="Stiri oficiale si informații la zi"
       content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, duis pretium."
       ctaText="Cele mai noi informații oficiale"
-      ctaOnClick={() => alert("Button Clicked")} />
-  </div>;
+      ctaOnClick={() => alert("Button Clicked")}
+    />
+  </div>
+);
 
-export const withHeaderOnly = () => 
-  <div style={{width: 500}}>
-    <InstrumentsItem 
-      color="#FF1616" 
-      title="Stiri oficiale si informații la zi" />
-  </div>;
+export const withHeaderOnly = () => (
+  <div style={{ width: 500 }}>
+    <InstrumentsItem
+      color="#FF1616"
+      title="Stiri oficiale si informații la zi"
+    />
+  </div>
+);
 
-export const withoutButton = () => 
-  <div style={{width: 500}}>
-    <InstrumentsItem 
-      color="#FF1616" 
-      title="Stiri oficiale si informații la zi" 
-      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, duis pretium." />
-  </div>;
+export const withoutButton = () => (
+  <div style={{ width: 500 }}>
+    <InstrumentsItem
+      color="#FF1616"
+      title="Stiri oficiale si informații la zi"
+      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, duis pretium."
+    />
+  </div>
+);
 
-export const withoutContent = () => 
-  <div style={{width: 500}}>
-    <InstrumentsItem 
-      color="#FF1616" 
-      title="Stiri oficiale si informații la zi" 
+export const withoutContent = () => (
+  <div style={{ width: 500 }}>
+    <InstrumentsItem
+      color="#FF1616"
+      title="Stiri oficiale si informații la zi"
       ctaText="Cele mai noi informații oficiale"
-      ctaOnClick={() => alert("Button Clicked")} />
-  </div>;
+      ctaOnClick={() => alert("Button Clicked")}
+    />
+  </div>
+);
