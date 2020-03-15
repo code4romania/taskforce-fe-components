103e985ff7a21cc8f02e99ddb827dde1a587161a
Alex Boicu <boicualexandru@mail.com>
2020-03-15 22:50:22 +0200

    InstrumentsItem component story fix

--- src/components/instruments-item/instruments-item.stories.js
+++ src/components/instruments-item/instruments-item.stories.js
@@ -5,43 +5,32 @@ import { InstrumentsItem } from "./instruments-item";
 export default { title: "InstrumentsItem" };
 
 export const basic = () => (
-  <div style={{ width: 500 }}>
-    <InstrumentsItem
-      color="#FF1616"
-      title="Stiri oficiale si informații la zi"
-      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, duis pretium."
-      ctaText="Cele mai noi informații oficiale"
-      ctaOnClick={() => alert("Button Clicked")}
-    />
-  </div>
+  <InstrumentsItem
+    color="#FF1616"
+    title="Stiri oficiale si informații la zi"
+    content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, duis pretium."
+    ctaText="Cele mai noi informații oficiale"
+    ctaOnClick={() => alert("Button Clicked")}
+  />
 );
 
 export const withHeaderOnly = () => (
-  <div style={{ width: 500 }}>
-    <InstrumentsItem
-      color="#FF1616"
-      title="Stiri oficiale si informații la zi"
-    />
-  </div>
+  <InstrumentsItem color="#FF1616" title="Stiri oficiale si informații la zi" />
 );
 
 export const withoutButton = () => (
-  <div style={{ width: 500 }}>
-    <InstrumentsItem
-      color="#FF1616"
-      title="Stiri oficiale si informații la zi"
-      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, duis pretium."
-    />
-  </div>
+  <InstrumentsItem
+    color="#FF1616"
+    title="Stiri oficiale si informații la zi"
+    content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, duis pretium."
+  />
 );
 
 export const withoutContent = () => (
-  <div style={{ width: 500 }}>
-    <InstrumentsItem
-      color="#FF1616"
-      title="Stiri oficiale si informații la zi"
-      ctaText="Cele mai noi informații oficiale"
-      ctaOnClick={() => alert("Button Clicked")}
-    />
-  </div>
+  <InstrumentsItem
+    color="#F5BCD6"
+    title="Stiri oficiale si informații la zi"
+    ctaText="Cele mai noi informații oficiale"
+    ctaOnClick={() => alert("Button Clicked")}
+  />
 );
