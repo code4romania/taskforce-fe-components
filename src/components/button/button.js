10cc32b73af57b00adeb137e55b382476135e82d
Utwo <mihai.legat@gmail.com>
2020-03-15 13:42:49 +0200

    Only transpile with babel.

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./../../styles.scss";
 
 export const Button = ({ children }) => {
   return <button className="button is-danger is-outlined">{children}</button>;
--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./../../styles.scss";
 import { Button } from "./button";
 
 export default { title: "Button" };
