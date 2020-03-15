5cb22daf18e01703770684375bc3b2e26aca3146
Radu Adascalitei <adascalitei.radu@live.com>
2020-03-16 00:30:02 +0200

    feat: search input (#19)
    * feat: search input
    
    * Update src/components/search-input/search-input.js
    
    Co-Authored-By: Dan Lepadatu <lepadatudan@gmail.com>
    
    * Update src/components/search-input/search-input.js
    
    Co-Authored-By: Dan Lepadatu <lepadatudan@gmail.com>
    
    * Update src/components/search-input/search-input.js
    
    Co-Authored-By: Dan Lepadatu <lepadatudan@gmail.com>
    
    * chore: fixed according to PR comments
    
    * chore: update onValueChange story
    
    * chore: added svg plugin
    
    * chore: fix class name
    
    Co-authored-by: Dan Lepadatu <lepadatudan@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import "./styles.scss";
 export * from "./components/button/button";
 export * from "./components/list-header/list-header";
 export * from "./components/social-share/social-share";
+export * from "./components/search-input/search-input";
