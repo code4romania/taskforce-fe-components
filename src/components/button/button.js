b7e6348568b3cc10309d6b46573cad1d9566b5c9
Surdu <neon-doubt@maskmail.net>
2020-03-15 20:20:39 +0200

    feat(button): Implemented the button component
    Fixes #8

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,10 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export const Button = ({ children }) => {
-  return <button className="button is-danger is-outlined">{children}</button>;
+export const Button = ({ children, onClick, type, inverted }) => {
+  const additionalClasses = [];
+
+  if (type) {
+    additionalClasses.push(`is-${type}`);
+  }
+
+  if (inverted) {
+    additionalClasses.push("is-outlined");
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      className={"button is-primary " + additionalClasses.join(" ")}
+    >
+      {children}
+    </button>
+  );
 };
 
 Button.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired,
+  type: PropTypes.oneOf(["primary", "danger"]),
+  inverted: PropTypes.bool
 };
