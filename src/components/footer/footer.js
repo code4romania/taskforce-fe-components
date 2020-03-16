1f3c99fed9e30dfed22a19a44cba5e1665ff40c9
Daniel <dansto13@yahoo.com>
2020-03-16 12:49:08 +0100

    Added improved Footer.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,45 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./../../styles.scss";
-import { Link } from "react-router-dom";
 import Code4RomaniaGrayLogo from "../../../images/code4romania-gray.svg";
 import "./footer.scss";
 
-const Footer = () => {
+const Footer = ({ children }) => {
   return (
     <footer className="App-footer">
       <div className="footer-content">
-        <ul className="useful-links">
-          <li>Linkuri utile</li>
-          <li>
-            <Link to="/termeni-si-conditii">Termeni și condiții</Link>
-          </li>
-          <li>
-            <Link to="/politica-confidentialitate">
-              Politică de confidențialitate
-            </Link>
-          </li>
-          <li>
-            <a
-              href="https://code4.ro/ro/codul-de-conduita/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Codul de Conduită
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://code4.ro/ro/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Code for Romania
-            </a>
-          </li>
-          <li>
-            <a href="mailto:contact@code4.ro">Contact</a>
-          </li>
-        </ul>
+        {children}
         <div className="code4-branding">
           <a
             href="https://code4.ro/ro/"
@@ -47,7 +16,7 @@ const Footer = () => {
             rel="noopener noreferrer"
           >
             {/* <img src={Code4romania_gray} alt="Code for Romania gray logo"></img> */}
-            <Code4RomaniaGrayLogo />
+            <Code4RomaniaGrayLogo style={{ height: 40 }} />
           </a>
           <p>© 2020 Code for Romania.</p>
           <p>
@@ -58,6 +27,10 @@ const Footer = () => {
       </div>
     </footer>
   );
+};
+
+Footer.propTypes = {
+  children: PropTypes.node.isRequired
 };
 
 export default Footer;
