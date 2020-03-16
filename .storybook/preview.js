86ed61e67445f572a7151143a57671374648877c
Daniel <dansto13@yahoo.com>
2020-03-16 12:50:06 +0100

    Removed unnecesarry preview files.

--- .storybook/preview.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { addDecorator } from "@storybook/react";
-import { MemoryRouter } from "react-router";
-
-addDecorator(story => (
-  <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
-));
