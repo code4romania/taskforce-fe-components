cee729ea1ada38e581d93e991d9e5473caad730f
Dan Lepadatu <lepadatudan@gmail.com>
2020-03-16 10:50:13 +0100

    feat: add List and ListItem components (#25)

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ export * from "./components/button/button";
 export * from "./components/list-header/list-header";
 export * from "./components/social-share/social-share";
 export * from "./components/search-input/search-input";
+export * from "./components/list/list";
+export * from "./components/list-item/list-item";
