a7342ed36dbb617a6eb81dc5b2f0e3b0f82a6aa3
Victor Bojica <victor@niftylearning.io>
2020-03-17 21:47:22 +0200

    updated partner logo;

--- src/components/partner-logo/partner-logo.js
+++ src/components/partner-logo/partner-logo.js
@@ -1,23 +1,13 @@
 import React from "react";
 
-import logoBig from "./assets/logo.png";
-import logoSmall from "./assets/logo-small.png";
+import logo from "./assets/logo.png";
 
 import "./partner-logo.scss";
 
 export const PartnerLogo = () => {
   return (
     <div className="partner-logo">
-      <img
-        src={logoBig}
-        className="partner-logo--big"
-        alt="Guvernul Romaniei"
-      />
-      <img
-        src={logoSmall}
-        className="partner-logo--small"
-        alt="Guvernul Romaniei"
-      />
+      <img src={logo} className="partner-logo" alt="Guvernul Romaniei" />
     </div>
   );
 };
