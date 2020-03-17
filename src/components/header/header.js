520dfbc3f8b51abdef4fe73f0c9e5bf66b66d75f
anca <anca.cismasiu@gmail.com>
2020-03-17 23:41:29 +0200

    Feature/header component (#34)
    * created Header component
    
    * renamed props
    
    * undo changes to eslintrc
    
    * fixed svg issues
    
    * removed old svgs
    
    * impelmented PR feedback
    
    * Update src/components/header/header.js
    
    Co-Authored-By: Nicu <neon-doubt@maskmail.net>
    
    Co-authored-by: Nicu <neon-doubt@maskmail.net>

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,42 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./header.scss";
-import GovLogoSVG from "../../icons/gov_logo.svg";
-import C4RLogoSvg from "../../icons/c4r_logo.svg";
 
 export const Header = ({ name, Logo, MenuItems, ProfileItems }) => {
   return (
     <header className="App-header">
       <div className="top-header">
         <div className="app-logo">{Logo}</div>
-        <div className="title">{name}</div>
+        <div className="title">{ name || ""}</div>
         <nav>
           {MenuItems}
           <span>{ProfileItems}</span>
         </nav>
-      </div>
-      <div className="bottom-header">
-        <div className="app-credits">
-          <span>Un proiect dezvoltat de</span>
-          <a href="https://code4.ro/" target="_blank" rel="noopener noreferrer">
-            <C4RLogoSvg />
-          </a>
-          <span>În parteneriat cu </span>
-          <a
-            href="https://www.gov.ro/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <GovLogoSVG />
-          </a>
-        </div>
       </div>
     </header>
   );
 };
 
 Header.propTypes = {
-  name: PropTypes.string.isRequired,
+  name: PropTypes.string,
   Logo: PropTypes.element.isRequired,
   MenuItems: PropTypes.array.isRequired,
   ProfileItems: PropTypes.array
