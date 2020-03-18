f9ef65f0e2c18ed300625e2c8a8915ceafe30ce2
Victor Bojica <victor@niftylearning.io>
2020-03-18 17:28:18 +0200

    remove default export for footer component;

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,7 @@ import "./../../styles.scss";
 import Code4RomaniaGrayLogo from "../../../images/code4romania-gray.svg";
 import "./footer.scss";
 
-const Footer = ({ children }) => {
+export const Footer = ({ children }) => {
   return (
     <footer className="App-footer">
       <div className="footer-content">
@@ -31,5 +31,3 @@ const Footer = ({ children }) => {
 Footer.propTypes = {
   children: PropTypes.node.isRequired
 };
-
-export default Footer;
