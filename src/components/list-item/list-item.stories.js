62d67e0b57c56597d81e233c09ac5c9de9553ce8
Dan Lepadatu <lepadatudan@gmail.com>
2020-03-18 20:52:59 +0100

    feat: add value prop to the listItem component

--- src/components/list-item/list-item.stories.js
+++ src/components/list-item/list-item.stories.js
@@ -1,12 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../../styles.scss";
 import { ListItem } from "./list-item";
 
 export default { title: "ListItem" };
-
-function onClick(e) {
-  console.log("onClick", e);
-}
 
 export const withInactiveState = () => (
   <ListItem title={"Simptome și tratament"} />
@@ -20,6 +16,43 @@ export const withActiveStateAndArrow = () => (
   <ListItem active={true} hasNext={true} title={"Simptome și tratament"} />
 );
 
-export const withInactiveStateOnClick = () => (
-  <ListItem title={"Simptome și tratament"} onClick={onClick} />
-);
+export const onClickWithValue = () => {
+  const [data, setData] = useState({});
+  const documentExample = {
+    title: "Ce spune legea",
+    doc_type: "STATIC_PAGE",
+    doc_id: 101,
+    active: true,
+    display_order: 2,
+    content: "Lorem Ipsum...",
+    nodes: null,
+    buttons: [
+      {
+        title: "Vezi toate legile",
+        link: "https://test.com"
+      }
+    ]
+  };
+
+  return (
+    <div style={{ minWidth: 300, maxWidth: 800 }}>
+      <pre style={{ whiteSpace: "pre-wrap" }}>{JSON.stringify(data)}</pre>
+      <ListItem
+        title={documentExample.title}
+        onClick={setData}
+        value={documentExample}
+      />
+    </div>
+  );
+};
+
+export const onClickWithWithoutValue = () => {
+  const [data, setData] = useState({});
+
+  return (
+    <div style={{ minWidth: 300, maxWidth: 800 }}>
+      <pre style={{ whiteSpace: "pre-wrap" }}>{JSON.stringify(data)}</pre>
+      <ListItem title={"Simptome și tratament"} onClick={setData} />
+    </div>
+  );
+};
