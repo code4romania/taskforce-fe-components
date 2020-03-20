f8e78a35ae861ddcb96e27de86809a6de0021942
Dan Lepadatu <lepadatudan@gmail.com>
2020-03-20 21:28:44 +0100

    feat: add nonOption state to the listItem component

--- src/components/list-item/list-item.js
+++ src/components/list-item/list-item.js
@@ -5,19 +5,27 @@ import Tick from "../../icons/tick.svg";
 import ArrowRight from "../../icons/arrow-right.svg";
 import "./list-item.styles.scss";
 
-export const ListItem = ({ title, active, hasNext, value, onClick }) => {
+export const ListItem = ({
+  title,
+  active,
+  hasNext,
+  value,
+  onClick,
+  nonOption
+}) => {
   const onClickCb = () => {
     if (onClick) onClick(value);
   };
 
   return (
     <div
-      className={classNames("__list-item", { "__list-item--active": active })}
+      className={classNames("__list-item", {
+        "__list-item--active": active && !nonOption,
+        "__list-item--non-option": nonOption
+      })}
       onClick={onClickCb}
     >
-      <div className="__list-item__left-side">
-        <Tick />
-      </div>
+      <div className="__list-item__left-side">{!nonOption && <Tick />}</div>
       <div className="__list-item__content">{title}</div>
       {hasNext && (
         <div className="__list-item__right-side">
@@ -30,6 +38,7 @@ export const ListItem = ({ title, active, hasNext, value, onClick }) => {
 
 ListItem.defaultProps = {
   active: false,
+  nonOption: false,
   hasNext: false
 };
 
@@ -38,5 +47,6 @@ ListItem.propTypes = {
   value: PropTypes.object,
   active: PropTypes.bool,
   hasNext: PropTypes.bool,
+  nonOption: PropTypes.bool,
   onClick: PropTypes.func
 };
