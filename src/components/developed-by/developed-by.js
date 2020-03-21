431d3ca74fa19792eb09d136a5c58bc1ba737e1d
Dan Lepadatu <lepadatudan@gmail.com>
2020-03-21 22:52:25 +0100

    styles: improve mobile styles for developed-by component

--- src/components/developed-by/developed-by.js
+++ src/components/developed-by/developed-by.js
@@ -10,39 +10,35 @@ export const DevelopedBy = ({ showSecondLine }) => {
   return (
     <>
       <div className="container developed-by">
-        <span>
-          <div>
-            <div className="caption">Un proiect în parteneriat cu </div>
-            <Logo
-              src={partnerLogo}
-              url={"https://www.gov.ro"}
-              alt={"Guvernul Romaniei Logo"}
-            />
-          </div>
+        <div>
+          <div classNamne="caption">Un proiect în parteneriat cu </div>
+          <Logo
+            src={partnerLogo}
+            url={"https://www.gov.ro"}
+            alt={"Guvernul Romaniei Logo"}
+          />
+        </div>
 
-          <div>
-            <div className="caption"> dezvoltat de </div>
-            <Logo
-              src={developerLogo}
-              url={"https://code4.ro"}
-              alt={"Code 4 Romania Logo"}
-              imgClass={"smaller"}
-            />
-          </div>
-        </span>
+        <div>
+          <div className="caption"> dezvoltat de </div>
+          <Logo
+            src={developerLogo}
+            url={"https://code4.ro"}
+            alt={"Code 4 Romania Logo"}
+            imgClass={"smaller"}
+          />
+        </div>
       </div>
       {showSecondLine && (
         <div className="container developed-by">
-          <span>
-            <div>
-              <div className="caption">Conținut avizat de</div>
-              <Logo
-                src={dsuLogo}
-                url={"http://www.dsu.mai.gov.ro/"}
-                alt={"Directia pentru Situatii de Urgenta Logo"}
-              />
-            </div>
-          </span>
+          <div>
+            <div className="caption">Conținut avizat de</div>
+            <Logo
+              src={dsuLogo}
+              url={"http://www.dsu.mai.gov.ro/"}
+              alt={"Directia pentru Situatii de Urgenta Logo"}
+            />
+          </div>
         </div>
       )}
     </>
