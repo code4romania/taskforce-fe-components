f3db4a5882646d98a012d7cd6d7e2bc65caa4d95
Dan Lepadatu <lepadatudan@gmail.com>
2020-03-22 13:41:24 +0100

    feat: add onClick callback to the sidebar-menu-item and add story example

--- src/components/sidebar-menu-item/sidebar-menu-item.js
+++ src/components/sidebar-menu-item/sidebar-menu-item.js
@@ -2,11 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./sidebar-menu-item.scss";
 
-export const SidebarMenuItem = ({ active, isTitle, children }) => {
+export const SidebarMenuItem = ({ active, isTitle, children, onClick }) => {
+  const onClickCb = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
+
   return (
     <li
       className={`__sidebar-menu-item ${active && "active"} ${isTitle &&
         "isTitle"}`}
+      onClick={onClickCb}
     >
       {children}
     </li>
@@ -16,6 +23,7 @@ export const SidebarMenuItem = ({ active, isTitle, children }) => {
 SidebarMenuItem.propTypes = {
   active: PropTypes.bool,
   isTitle: PropTypes.bool,
+  onClick: PropTypes.func,
   children: PropTypes.node.isRequired
 };
 
--- src/components/sidebar-menu-item/sidebar-menu-item.stories.js
+++ src/components/sidebar-menu-item/sidebar-menu-item.stories.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../../styles.scss";
 import { SidebarMenuItem } from "./sidebar-menu-item";
 import { withKnobs } from "@storybook/addon-knobs";
@@ -8,6 +8,7 @@ export default { title: "Sidebar Menu Item", decorators: [withKnobs] };
 export const sectionTitle = () => (
   <SidebarMenuItem isTitle>Transmitere și simptome</SidebarMenuItem>
 );
+
 export const activeSectionTitle = () => (
   <SidebarMenuItem isTitle active>
     Transmitere și simptome
@@ -21,3 +22,31 @@ export const subSection = () => (
 export const activeSubSection = () => (
   <SidebarMenuItem active>Transmitere și simptome</SidebarMenuItem>
 );
+
+export const sectionTitleWithCallback = () => {
+  const [data, setData] = useState({});
+  const documentExample = {
+    title: "Ce spune legea",
+    doc_type: "STATIC_PAGE",
+    doc_id: 101,
+    active: true,
+    display_order: 2,
+    content: "Lorem Ipsum...",
+    nodes: null,
+    buttons: [
+      {
+        title: "Vezi toate legile",
+        link: "https://test.com"
+      }
+    ]
+  };
+
+  return (
+    <div style={{ minWidth: 300, maxWidth: 800 }}>
+      <pre style={{ whiteSpace: "pre-wrap" }}>{JSON.stringify(data)}</pre>
+      <SidebarMenuItem isTitle onClick={() => setData(documentExample)}>
+        Transmitere și simptome
+      </SidebarMenuItem>
+    </div>
+  );
+};
