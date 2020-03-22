512f6e9b2d01e6845c2e3aa5b3a6e2cc9bf49ee3
Utwo <mihai.legat@gmail.com>
2020-03-22 17:28:16 +0200

    Allow png, jpeg and svg in logo component. Replace pngs with svg where possible.

--- src/components/developed-by/developed-by.js
+++ src/components/developed-by/developed-by.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import DeveloperLogo from "../../images/code4romania.svg";
 import partnerLogo from "./assets/partener.png";
-import developerLogo from "./assets/developer.png";
 import dsuLogo from "./assets/dsu.png";
 import "./developed-by.scss";
 import { Logo } from "../logo/logo";
@@ -12,32 +12,25 @@ export const DevelopedBy = ({ showSecondLine }) => {
       <div className="container developed-by">
         <div>
           <div classNamne="caption">Un proiect în parteneriat cu </div>
-          <Logo
-            src={partnerLogo}
-            url={"https://www.gov.ro"}
-            alt={"Guvernul Romaniei Logo"}
-          />
+          <Logo url={"https://www.gov.ro"}>
+            <img src={partnerLogo} alt="Guvernul Romaniei" />
+          </Logo>
         </div>
 
         <div>
           <div className="caption"> dezvoltat de </div>
-          <Logo
-            src={developerLogo}
-            url={"https://code4.ro"}
-            alt={"Code 4 Romania Logo"}
-            imgClass={"smaller"}
-          />
+          <Logo url={"https://code4.ro"} imgClass={"smaller"}>
+            <DeveloperLogo width="110" />
+          </Logo>
         </div>
       </div>
       {showSecondLine && (
         <div className="container developed-by">
           <div>
             <div className="caption">Conținut avizat de</div>
-            <Logo
-              src={dsuLogo}
-              url={"http://www.dsu.mai.gov.ro/"}
-              alt={"Directia pentru Situatii de Urgenta Logo"}
-            />
+            <Logo url={"http://www.dsu.mai.gov.ro/"}>
+              <img src={dsuLogo} alt="Directia pentru Situatii de Urgenta" />
+            </Logo>
           </div>
         </div>
       )}
