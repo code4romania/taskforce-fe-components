428b388e7b1b6219b8b3ad02de2def456e0c7515
Andrei Cioban <andrei.cioban@assist.ro>
2020-03-23 12:23:35 +0200

    Redirect flow (#88)
    * Make style simillar with what's on stiri oficiale
    
    * use redirect flow
    
    * bump patch
    
    * Update src/components/subscribe/subscribe-form.js
    
    Co-Authored-By: Dan Lepadatu <lepadatudan@gmail.com>
    
    * Bump patch + remove unused dependency
    
    * updated package-lock
    
    * Decrease patch. Regenrate package-lock
    
    Co-authored-by: Dan Lepadatu <lepadatudan@gmail.com>

--- src/components/subscribe/mailchimp-subscribe.js
+++ src/components/subscribe/mailchimp-subscribe.js
@@ -1,33 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { SubscribeForm } from "./subscribe-form";
-import { sendSubscriptionRequest } from "./mailchimp.service";
 
 export const MailchimpSubscribe = ({ url, compact }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [isSuccess, setIsSuccess] = useState();
-  const [errorMessage, setErrorMessage] = useState();
-
-  const subscribe = async ({ email }) => {
-    setIsLoading(true);
-    try {
-      await sendSubscriptionRequest(url, email);
-      setIsSuccess(true);
-    } catch (errorMsg) {
-      setErrorMessage(errorMsg);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   return (
-    <SubscribeForm
-      onSubmit={formData => subscribe(formData)}
-      loading={isLoading}
-      success={isSuccess}
-      errorMessage={errorMessage}
-      compact={compact}
-    />
+    <form method="post" target="_blank" action={url}>
+      <SubscribeForm compact={compact} />
+    </form>
   );
 };
 
