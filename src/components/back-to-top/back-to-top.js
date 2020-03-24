a9ba9be79ddf233ec2600e6dc1cc05836b1e0f65
Daniel <dansto13@yahoo.com>
2020-03-24 20:19:07 +0100

    Add back to top component.

---
+++ src/components/back-to-top/back-to-top.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ArrowUp from "../../images/icons/circle-up.svg";
+import "./back-to-top.scss";
+const BackToTop = ({ width, height }) => {
+  return (
+    <div
+      className="back-to-top"
+      onClick={window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      })}
+    >
+      <ArrowUp width={width} height={height} />
+    </div>
+  );
+};
+export default BackToTop;
+BackToTop.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number
+};
+BackToTop.defaultProps = {
+  width: 40,
+  height: 40
+};
---
+++ src/components/back-to-top/back-to-top.stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+import BackToTop from "./back-to-top";
+
+export default { title: "Back To Top" };
+export const Default = () => <BackToTop />;
+export const withSizes = () => <BackToTop width={100} height={100} />;
