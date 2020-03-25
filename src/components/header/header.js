94634c179cf863c37cc91909c99cc4185ceca585
dmfarcas <dan@farcas.me>
2020-03-25 20:23:42 +0200

    dmfarcas: code review

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./header.scss";
-import useResizer from "../../utils/hooks/useResizer";
 
 import Hamburger from "../../images/icons/hamburger.svg";
 import HamburgerOpen from "../../images/icons/hamburger-open.svg";
 
 export const Header = ({ name, Logo, MenuItems, ProfileItems }) => {
-  const [hamburgerOpened, hamburgerOnClick] = useState(false);
-  const isMobile = useResizer();
+  const [hamburgerOpened, hamburgerOnClick] = useState(true);
   const HamburgerIcon = hamburgerOpened ? HamburgerOpen : Hamburger;
 
   return (
@@ -17,17 +15,17 @@ export const Header = ({ name, Logo, MenuItems, ProfileItems }) => {
         {Logo}
         {name && <span className="App-header__title">{name}</span>}
       </div>
-      {isMobile && (
+      {
         <div className="App-header__hamburger">
           <HamburgerIcon onClick={() => hamburgerOnClick(!hamburgerOpened)} />
         </div>
-      )}
-      {(!isMobile || hamburgerOpened) && (
-        <nav>
-          {MenuItems}
-          {ProfileItems}
-        </nav>
-      )}
+      }
+      {/* {hamburgerOpened && ( */}
+      <nav className={!hamburgerOpened ? "App-header__nav--hidden" : ""}>
+        {MenuItems}
+        {ProfileItems}
+      </nav>
+      {/* )} */}
     </header>
   );
 };
