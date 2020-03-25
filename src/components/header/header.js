f4eadb3b0ae21f080f2127e213f355bb57a5916c
Dan Farcas <dmfarcas@users.noreply.github.com>
2020-03-25 21:07:05 +0200

    dmfarcas: hamburger mobile menu and banner fix (#102)
    * dmfarcas: hamburger mobile menu and banner fix
    
    * dmfarcas: used eslint
    
    * Update src/components/banner/banner.scss
    
    Co-Authored-By: Dan Lepadatu <lepadatudan@gmail.com>
    
    * dmfarcas: code review
    
    * dmfarcas: code review
    
    Co-authored-by: Dan Lepadatu <lepadatudan@gmail.com>

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./header.scss";
 
+import Hamburger from "../../images/icons/hamburger.svg";
+import HamburgerOpen from "../../images/icons/hamburger-open.svg";
+
 export const Header = ({ name, Logo, MenuItems, ProfileItems }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const HamburgerIcon = isOpen ? HamburgerOpen : Hamburger;
+
   return (
     <header className="App-header container">
       <div className="App-header__logo">
         {Logo}
-        {name ? <span className="App-header__title">{name}</span> : ""}
+        {name && <span className="App-header__title">{name}</span>}
       </div>
-      <nav>
+      <div className="App-header__menu-icon">
+        <HamburgerIcon onClick={() => setIsOpen(!isOpen)} />
+      </div>
+      <nav className={!isOpen ? "App-header__nav--hidden" : ""}>
         {MenuItems}
         {ProfileItems}
       </nav>
