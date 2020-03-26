a92a8f1e9b002b4bddbf9ebbfe20d7214836919a
Daniel <dansto13@yahoo.com>
2020-03-26 20:03:35 +0100

    Add opacity to Back to Top.

--- src/components/back-to-top/back-to-top.js
+++ src/components/back-to-top/back-to-top.js
@@ -2,21 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 import ArrowUp from "../../images/icons/circle-up.svg";
 import "./back-to-top.scss";
+
 const BackToTop = ({ width, height }) => {
   return (
     <div
       className="back-to-top"
-      onClick={window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-      })}
+      onClick={() =>
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth"
+        })
+      }
     >
       <ArrowUp width={width} height={height} />
     </div>
   );
 };
 export default BackToTop;
+
 BackToTop.propTypes = {
   width: PropTypes.number,
   height: PropTypes.number
