e1e5cb4cf2b804cd973ff8a631780c954d7d43ca
Mihaela Topirceanu <mihaela.ilin@yahoo.com>
2020-03-28 21:02:25 +0200

    DevelopedBy component with parameters (#121)
    * Adapt the DevelopedBy component to receive external partner logos and second line content + disable partners section on demand.
    
    * DevelopedBy: showPartners is default true.
    
    * DevelopedBy component: add wrapper div to developed by section.
    
    * styles: fix logos alignment for non-desktop devices
    
    Co-authored-by: Mihaela Topirceanu <mihaela.topirceanu@haufe-lexware.com>
    Co-authored-by: Dan Lepadatu <lepadatudan@gmail.com>

--- src/components/developed-by/developed-by.stories.js
+++ src/components/developed-by/developed-by.stories.js
@@ -2,9 +2,56 @@ import React from "react";
 import "./../../styles.scss";
 import { DevelopedBy } from "./developed-by";
 import { withKnobs } from "@storybook/addon-knobs";
+import { Logo } from "../..";
 
 export default { title: "Developed By", decorators: [withKnobs] };
 
 export const oneLine = () => <DevelopedBy />;
 
-export const twoLines = () => <DevelopedBy showSecondLine={true} />;
+export const twoLines = () => <DevelopedBy showSecondLine />;
+
+export const oneLineNoPartners = () => <DevelopedBy showPartners={false} />;
+
+const customPartnerLogos = [
+  <Logo url={"https://girlswhocode.com/"} key="github_1">
+    <img
+      src="http://www.bigdreammovement.com/wp-content/uploads/girls-who-code.png"
+      alt="Custom Partner"
+    />
+  </Logo>,
+  <Logo url={"https://github.com"} key="github_2">
+    <img
+      src="https://tctechcrunch2011.files.wordpress.com/2013/01/github-logo.png"
+      alt="Custom Partner"
+    />
+  </Logo>
+];
+
+const customSecondLineLogos = [
+  <Logo url={"https://codeforall.org/"} key="github_3">
+    <img
+      src="https://epf.org.pl/en/wp-content/uploads/sites/3/2015/11/code_for_all_logo.png"
+      alt="Custom Partner"
+    />
+  </Logo>,
+  <Logo url={"https://girlswhocode.com/"} key="github_4">
+    <img
+      src="http://www.bigdreammovement.com/wp-content/uploads/girls-who-code.png"
+      alt="Custom Partner"
+    />
+  </Logo>
+];
+
+export const oneLineCustom = () => (
+  <DevelopedBy showPartners partnerLogos={customPartnerLogos} />
+);
+
+export const twoLineCustom = () => (
+  <DevelopedBy
+    showPartners
+    partnerLogos={customPartnerLogos}
+    showSecondLine
+    secondLineCaption="Much appreciation goes to"
+    secondLineLogos={customSecondLineLogos}
+  />
+);
