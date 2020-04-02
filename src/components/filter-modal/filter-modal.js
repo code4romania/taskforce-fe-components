87c86a47286f281877f5bde41e3e286f972c068f
Mot, Ioana <ioana.mot@haufe-lexware.com>
2020-04-02 11:13:43 +0300

    Removed arrows used as scrollbars

--- src/components/filter-modal/filter-modal.js
+++ src/components/filter-modal/filter-modal.js
@@ -4,8 +4,6 @@ import "./filter-modal.scss";
 
 import { SearchInput } from "../search-input/search-input";
 
-import ChevronUpIcon from "../../images/icons/chevron-up.svg";
-
 export const FilterModal = ({ placeholder, values, isOpen, selectValue }) => {
   const [selectedValue, setSelectedValue] = useState("");
 
@@ -23,10 +21,6 @@ export const FilterModal = ({ placeholder, values, isOpen, selectValue }) => {
     return null;
   }
 
-  const WithScrollDiv = WithScrollHandler(({ children, ...props }) =>
-    React.createElement("div", props, children)
-  );
-
   return (
     <div className="filter-modal__container">
       <div className="filter-modal">
@@ -38,37 +32,25 @@ export const FilterModal = ({ placeholder, values, isOpen, selectValue }) => {
         />
         <div className="filter-modal__separator" />
         <div className="filter-modal__scrollable-wrapper">
-          <WithScrollDiv className="filter-modal__list">
-            {(hasReachedTop, hasReachedBottom) => (
-              <>
-                {values
-                  .filter(
-                    value =>
-                      !selectedValue ||
-                      value.toLowerCase().includes(selectedValue.toLowerCase())
-                  )
-                  .map(value => (
-                    <div
-                      key={`FilterModalValue${value}`}
-                      className="filter-modal__value"
-                      onClick={() => onValueSelectedHandler(value)}
-                    >
-                      {value}
-                    </div>
-                  ))}
-                <div className="filter-modal__scrollbar">
-                  <span className="icon">
-                    {!hasReachedTop && <ChevronUpIcon />}
-                  </span>
-                  <span className="icon">
-                    {!hasReachedBottom && (
-                      <ChevronUpIcon className="filter-modal__scrollbar--down" />
-                    )}
-                  </span>
-                </div>
-              </>
-            )}
-          </WithScrollDiv>
+          <div className="filter-modal__list">
+            <>
+              {values
+                .filter(
+                  value =>
+                    !selectedValue ||
+                    value.toLowerCase().includes(selectedValue.toLowerCase())
+                )
+                .map(value => (
+                  <div
+                    key={`FilterModalValue${value}`}
+                    className="filter-modal__value"
+                    onClick={() => onValueSelectedHandler(value)}
+                  >
+                    {value}
+                  </div>
+                ))}
+            </>
+          </div>
         </div>
       </div>
     </div>
@@ -86,54 +68,4 @@ FilterModal.propTypes = {
   placeholder: PropTypes.string,
   values: PropTypes.array,
   selectValue: PropTypes.func
-};
-
-const WithScrollHandler = WrappedComponent => {
-  const WrapperComponent = ({ children, ...rest }) => {
-    const [contentHeight, setContentHeight] = useState(0);
-    const [hasReachedTop, setHasReachedTop] = useState(true);
-    const [hasReachedBottom, setHasReachedBottom] = useState(false);
-
-    const onScrollHandler = event => {
-      let updatedContentHeight = contentHeight || 0;
-
-      if (!updatedContentHeight) {
-        event.target.children.forEach(child => {
-          if (child.className.includes("filter-modal__value")) {
-            updatedContentHeight += child.offsetHeight;
-          }
-        });
-
-        setContentHeight(updatedContentHeight);
-      }
-
-      const target = event.target,
-        scrollTop = target.scrollTop,
-        height = target.offsetHeight;
-
-      const currentHasReachedTop = scrollTop === 0;
-      const currentHasReachedBottom =
-        scrollTop + height === updatedContentHeight;
-
-      if (currentHasReachedTop !== hasReachedTop) {
-        setHasReachedTop(currentHasReachedTop);
-      }
-
-      if (currentHasReachedBottom !== hasReachedBottom) {
-        setHasReachedBottom(currentHasReachedBottom);
-      }
-    };
-
-    return (
-      <WrappedComponent {...rest} onScroll={onScrollHandler}>
-        {children(hasReachedTop, hasReachedBottom)}
-      </WrappedComponent>
-    );
-  };
-
-  WrapperComponent.propTypes = {
-    children: PropTypes.func
-  };
-
-  return WrapperComponent;
 };
