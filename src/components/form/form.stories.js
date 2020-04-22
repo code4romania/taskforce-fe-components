ae64f73988c185ed478e39ac3062169066f23ec6
Mircea M <mircea.moise@gmail.com>
2020-04-22 16:00:50 +0100

    Adding the ability to use the default next question on the question

--- src/components/form/form.stories.js
+++ src/components/form/form.stories.js
@@ -8,11 +8,7 @@ import { Form } from "./form";
 export default { title: "Form" };
 
 let evaluate = function(formData, options) {
-  if (formData[1]) {
-    return options[1];
-  } else {
-    return options[0];
-  }
+  return options[0];
 };
 export const withCeMaFacData = () => (
   <Form
