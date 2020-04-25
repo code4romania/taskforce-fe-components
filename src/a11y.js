34ef584d68fef2e65d0e5330141a2cf4fbb22ce2
Dan Manastireanu <498419+danmana@users.noreply.github.com>
2020-04-25 20:05:28 +0300

    feat: Make list-item accessible by keyboard. Closes #168

---
+++ src/a11y.js
@@ -0,0 +1,20 @@
+/**
+ * Wrapps a callback function in an event handler that responds to Enter or Space keys.
+ * To be used with onKeyPress/onKeyDown/onKeyUp
+ * @example
+ * <div onClick={() => doSomething()} onKeyPress={onEnterOrSpace(() => doSomething()))}>
+ * @param {() => any} callback
+ * @returns {React.KeyboardEventHandler}
+ */
+export function onEnterOrSpace(callback) {
+  return event => {
+    if (event.key === "Enter" || event.key === " ") {
+      if (typeof callback === "function") {
+        callback();
+        // we already handled this event we can stop the browser default
+        event.preventDefault();
+        event.stopPropagation();
+      }
+    }
+  };
+}
