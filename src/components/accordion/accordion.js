bb22dfe85495933fe2d1e53920d6c336665735d0
Andrei <33538982+andreibesleaga@users.noreply.github.com>
2020-04-27 20:28:44 +0300

    feat: SidebarMenu and Accordion components accesibility,  (#175)
    * added SidebarMenu accesibility
    
    * fix added import onEnterOrSpace
    
    * feat: make accordion component accesible for keyboard navigation
    
    * removed unused parameter
    
    * bugfix: eslint errors
    
    Co-authored-by: Andrei Besleaga <andrei.besleaga@tonica.ro>

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -3,17 +3,19 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 import ArrowDown from "../../images/icons/arrow-down.svg";
 import "./accordion.scss";
+import { onEnterOrSpace } from "../../a11y";
 
 export const Accordion = ({ title, content, children }) => {
   const [showContent, setShowContent] = useState(false);
 
   return (
-    <div className="message __accordion">
+    <div className="message __accordion" tabIndex="0">
       <div
         className={classNames("message-header __accordion-header", {
           "__accordion-header--expanded": showContent
         })}
         onClick={() => setShowContent(!showContent)}
+        onKeyPress={onEnterOrSpace(setShowContent(!showContent))}
       >
         <span className="__accordion-title">{title}</span>
         <div className="__accordion-arrow">
