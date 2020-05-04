304fafd387bb38a21d88bf2d2dfaf98832492e84
Dan Lepadatu <lepadatudan@gmail.com>
2020-05-04 18:30:13 +0200

    refactor: reverse temperature chart axes

--- src/components/temperature-chart/temperature-chart.stories.js
+++ src/components/temperature-chart/temperature-chart.stories.js
@@ -24,6 +24,26 @@ const results = [
     id: "140",
     date: "1585409621",
     temperature: 38.6
+  },
+  {
+    id: "134",
+    date: "1585409321",
+    temperature: 36.6
+  },
+  {
+    id: "136",
+    date: "1585409421",
+    temperature: 36
+  },
+  {
+    id: "139",
+    date: "1585409521",
+    temperature: 38.6
+  },
+  {
+    id: "140",
+    date: "1585409621",
+    temperature: 38.6
   }
 ];
 
