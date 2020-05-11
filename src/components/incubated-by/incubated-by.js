0202716b0610ee55f2915684f606dcfe87b7839d
Ciprian Platica <platica.ciprian@gmail.com>
2020-05-11 21:23:21 +0300

    Add support for 'partnerLogos' to IncubatedBy component

--- src/components/incubated-by/incubated-by.js
+++ src/components/incubated-by/incubated-by.js
@@ -1,17 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Logo } from "../logo/logo";
 import TaskforceLogo from "../../images/code4romania-color.svg";
 import "./incubated-by.scss";
 
-export const IncubatedBy = () => {
+export const IncubatedBy = ({ partnerLogos }) => {
   return (
     <div className="incubated-by-wrapper">
       <div className="incubated-by container">
-        <p className="caption">proiect dezvoltat în programul</p>
-        <Logo url="https://code4.ro">
-          <TaskforceLogo width="102" />
-        </Logo>
+        <div className="row">
+          <p className="caption">proiect dezvoltat în programul</p>
+          <Logo url="https://code4.ro">
+            <TaskforceLogo width="102" />
+          </Logo>
+        </div>
+        {partnerLogos && (
+          <div className="row">
+            <p className="caption">cu sprijinul</p>
+            <div className="logos">{partnerLogos}</div>
+          </div>
+        )}
       </div>
     </div>
   );
+};
+
+IncubatedBy.propTypes = {
+  partnerLogos: PropTypes.arrayOf(PropTypes.node)
 };
