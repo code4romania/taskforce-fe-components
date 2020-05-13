1f31020c484dec1df07eee65069e91e0004527e3
Cipick <platica.ciprian@gmail.com>
2020-05-13 11:05:27 +0300

    Add support for 'partnerLogos' to IncubatedBy component (#190)
    * Add support for 'partnerLogos' to IncubatedBy component
    
    * Update Patria-bank to transparent png
    
    * Compress image
    
    * Bump package version

--- src/components/incubated-by/incubated-by.stories.js
+++ src/components/incubated-by/incubated-by.stories.js
@@ -1,8 +1,39 @@
 import React from "react";
 import { IncubatedBy } from "./incubated-by";
+import { Logo } from "../..";
+import patriaBank from "../../images/patria-bank.png";
 
 import "./../../styles.scss";
+
+const customPartnerLogos = (
+  <Logo url="https://www.patriabank.ro/" key="patria-bank">
+    <img src={patriaBank} alt="Patria Bank" />
+  </Logo>
+);
+
+const customMultiplePartnersLogos = [
+  <Logo url={"https://codeforall.org/"} key="github_3">
+    <img
+      src="https://epf.org.pl/en/wp-content/uploads/sites/3/2015/11/code_for_all_logo.png"
+      alt="Custom Partner"
+    />
+  </Logo>,
+  <Logo url={"https://girlswhocode.com/"} key="github_4">
+    <img
+      src="http://www.bigdreammovement.com/wp-content/uploads/girls-who-code.png"
+      alt="Custom Partner"
+    />
+  </Logo>
+];
 
 export default { title: "Incubated By" };
 
 export const normal = () => <IncubatedBy />;
+
+export const withPartner = () => (
+  <IncubatedBy partnerLogos={customPartnerLogos} />
+);
+
+export const withMultiplePartners = () => (
+  <IncubatedBy partnerLogos={customMultiplePartnersLogos} />
+);
