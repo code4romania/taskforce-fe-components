5d7e5e455449d21a7fbb61e963def6325a1b9fc5
Utwo <mihai.legat@gmail.com>
2020-07-10 01:55:45 +0300

    Try to resolve scss loading error

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,21 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../src/**/*.stories.[tj]s"],
   addons: [
     "@storybook/react",
-    "@storybook/preset-scss",
+    // "@storybook/preset-scss",
     "@storybook/addon-docs",
     "@storybook/addon-storysource",
     "@storybook/addon-knobs/register",
-    "@storybook/addon-viewport/register"
-  ]
+    "@storybook/addon-viewport/register",
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../"),
+    });
+    return config;
+  },
 };
